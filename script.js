372d714986d936dd63b52748385bb9be0922ab69
Felipe Fernandes <felipedfe@gmail.com>
2021-12-12 17:45:04 -0300

    requisito 13

--- script.js
+++ script.js
@@ -8,29 +8,32 @@ function adicionaTarefa() {
   let novaTarefa = document.createElement("li");
   let listaTarefas = document.getElementById("lista-tarefas");
   novaTarefa.innerText = input.value;
-  novaTarefa.addEventListener("click", mudaCor);      // já adiciona a função pra mudar cor
+  novaTarefa.addEventListener("click", selecionaTarefa);      // já adiciona a função pra mudar cor
   novaTarefa.addEventListener("dblclick", riscaItem);  // add função pra riscar item
   listaTarefas.appendChild(novaTarefa);
   input.value = "";
 }
 
-// Mudando a cor de item da lista
-function mudaCor(evento) {
+// Seleciona item da lista (muda cor do fundo)
+function selecionaTarefa(evento) {
   let itemClicado = evento.target;
   let itensLista = document.getElementsByTagName("li");
   for (item of itensLista) {
     item.style.backgroundColor = "";          // aqui nesse FOR o programa apaga o style dos itens primeiro
+    item.classList.remove("selecionado");
   }
   itemClicado.style.backgroundColor = "rgb(128,128,128)";
+  itemClicado.classList.add("selecionado");
+  return itemClicado;
 }
 
 // Riscando itens
 function riscaItem(evento) {
   let itemClicado = evento.target;
-  if (itemClicado.className === "completed") {
-    itemClicado.className = "";
+  if (itemClicado.classList.contains("completed")) {
+    itemClicado.classList.remove("completed");
   }else {
-    itemClicado.className = "completed";
+    itemClicado.classList.add("completed");
   }
 }
 
@@ -54,10 +57,42 @@ botaoApagaRiscados.addEventListener("click", apagaRiscados);
 function apagaRiscados() {
   let tarefas = document.getElementsByTagName("li");
   for (i = 0; i < tarefas.length; i += 1) {
-    if (tarefas[i].className === "completed") {
-      console.log("oi")
+    if (tarefas[i].classList.contains("completed")) {
       tarefas[i].remove();
       i -= 1                // isso é pro loop voltar um elemento, pois quando um é removido o próximo ocupa a posição dele
     }
   }
 }
+
+// Botão para mover elemento (CIMA)
+
+let botaoMoveParaCima = document.getElementById("mover-cima");
+botaoMoveParaCima.addEventListener("click", moveCima);
+
+function moveCima() {
+  let listaTarefas = document.getElementById("lista-tarefas");
+  let tarefas = document.getElementById("lista-tarefas").children;
+  for (let i = 1; i < tarefas.length; i += 1) {         // i = 1 porque na posição 0 o elemento não se mexe
+    if (tarefas[i].classList.contains("selecionado")) {
+      listaTarefas.insertBefore(tarefas[i], listaTarefas.childNodes[i - 1]);
+      }
+    }
+  }
+  
+  // Botão para mover elemento (BAIXO)
+  
+  let botaoMoveParaBaixo = document.getElementById("mover-baixo");
+  botaoMoveParaBaixo.addEventListener("click", moveBaixo);
+
+  function moveBaixo() {
+    let listaTarefas = document.getElementById("lista-tarefas");
+    let tarefas = document.getElementById("lista-tarefas").children;
+    for (let i = tarefas.length - 2; i > -1; i -= 1) {    // i = tarefas.length - 2 porque isso corresponde ao último elemento do array - 1 
+      if (tarefas[i].classList.contains("selecionado")) {
+        listaTarefas.insertBefore(listaTarefas.childNodes[i + 1], tarefas[i]);
+        console.log("oi")
+        }
+    }
+
+  }
+
